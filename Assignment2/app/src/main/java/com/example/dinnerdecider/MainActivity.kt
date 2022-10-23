951fb20efe968d4f190a6894db27ea896c6abb83
yigit bediskan <yigitbediskan@gmail.com>
2022-10-23 16:14:15 -0400

    Screen shots are added

--- Assignment2/app/src/main/java/com/example/dinnerdecider/MainActivity.kt
+++ Assignment2/app/src/main/java/com/example/dinnerdecider/MainActivity.kt
@@ -10,32 +10,31 @@ class MainActivity : AppCompatActivity() {
 
     var foods: ArrayList<String> =
         arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese", "Turkish");
-    private lateinit var foodText: TextView;
-    private lateinit var newFood: EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         var addFood: Button = findViewById(R.id.addFoodButton);
         var decide: Button = findViewById(R.id.decideButton);
-        foodText = findViewById(R.id.foodText);
-        newFood = findViewById(R.id.newFood);
+        var foodText: TextView = findViewById(R.id.foodText);
+        var newFood: EditText = findViewById(R.id.newFood);
 
         addFood.setOnClickListener {
-            addFoodToList();
+            addFoodToList(newFood);
         }
 
         decide.setOnClickListener {
-            decide();
+            decide(foodText);
         }
 
     }
 
-    private fun decide() {
+    private fun decide(foodText: TextView) {
         foodText.text = foods.random();
     }
 
-    private fun addFoodToList() {
+    private fun addFoodToList(newFood: EditText) {
 
         if (!newFood.text.toString().equals("") && !foods.contains(newFood.text.toString())) {
             foods.add(newFood.text.toString())
